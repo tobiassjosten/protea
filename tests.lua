3fb6b09f699124595b4b5327b8952fbd12f49a16
Tobias Sjösten <tobias.sjosten@gmail.com>
2009-11-25 20:35:20 +0100

    Fixed some typos in the test suite

--- tests.lua
+++ tests.lua
@@ -396,14 +396,14 @@ end
 function TestStateQueue()
 	state:Queue('test', true)
 	state:Parse()
-	assert_true(state:Get('test'), 'State queue was not parsed')
+	assert_true(state:Get('test'), 'State queue was not parsed.')
 end
 
 function TestStateQueue()
 	state:Queue('test', true)
 	state:Dequeue('test')
 	state:Parse()
-	assert_false(state:Get('test'), 'State could not be removed from queue')
+	assert_false(state:Get('test'), 'State could not be removed from queue.')
 end
 
 
@@ -495,14 +495,14 @@ function TestGeoPathing()
 	geo:Load(sample_map)
 	local path = geo:Path(geo:Room(1), geo:Room(3))
 	assert_table(path, 'Did not get a propery path.')
-	assert_equal('east', path[1], 'First direction in path should be east')
-	assert_equal('east', path[2], 'Second direction in path should be east')
+	assert_equal('east', path[1], 'First direction in path should be east.')
+	assert_equal('east', path[2], 'Second direction in path should be east.')
 end
 
 function TestGeoPathingReverse()
 	geo:Load(sample_map)
 	local path = geo:Path(geo:Room(3), geo:Room(1))
 	assert_table(path, 'Did not get a propery path.')
-	assert_equal('west', path[1], 'First direction in path should be west')
-	assert_equal('west', path[2], 'Second direction in path should be west')
+	assert_equal('west', path[1], 'First direction in path should be west.')
+	assert_equal('west', path[2], 'Second direction in path should be west.')
 end
